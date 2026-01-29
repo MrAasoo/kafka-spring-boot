0b1b59b34c6b881880a6fdfcebf0c05a8355a481
Himanshu <himanshu1061999@gmail.com>
2026-01-29 16:04:02 +0530

    read me notes

--- src/main/java/com/aasoo/kafka/constant/KafkaConstants.java
+++ src/main/java/com/aasoo/kafka/constant/KafkaConstants.java
@@ -1,6 +1,5 @@
 package com.aasoo.kafka.constant;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
--- src/main/java/com/aasoo/kafka/controller/ChatController.java
+++ src/main/java/com/aasoo/kafka/controller/ChatController.java
@@ -6,7 +6,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Controller to handle chat messages and send them to Kafka.
